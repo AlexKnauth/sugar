a143950f229292f8cbc170a30339864c9653e7e9
Matthew Butterick <mbutterick@users.noreply.github.com>
2015-04-02 15:10:22 -0700

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '(("base" #:version "6.0")
+(define deps '("base"
                "typed-racket-lib"
                "typed-racket-more"))
 (define build-deps '("scribble-lib"))
