8253d7f24b87e7673f9898fc4017f3f12dbc7a1f
Matthew Butterick <mb@mbtype.com>
2015-04-03 18:13:39 -0700

    typed cache

--- sugar/cache.rkt
+++ sugar/cache.rkt
@@ -1,16 +1,6 @@
 #lang racket/base
-(require (for-syntax racket/base) racket/contract)
-(provide (all-defined-out))
+(require sugar/define)
+(require-via-wormhole "../typed/sugar/cache.rkt")
 
-(define/contract (make-caching-proc base-proc) 
-  (procedure? . -> . procedure?)
-  (let ([cache (make-hash)])
-    (λ args
-      (hash-ref! cache args (λ () (apply base-proc args))))))
-
-(define-syntax (define/caching stx)
-  (syntax-case stx ()
-    [(_ (name arg ... . rest-arg) body ...)
-     #'(define/caching name (λ(arg ... . rest-arg) body ...))]
-    [(_ name body ...)
-     #'(define name (make-caching-proc body ...))]))
+(provide+safe [make-caching-proc (procedure? . -> . procedure?)]
+              define/caching)
