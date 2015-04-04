8590932144b27fa4d51329a973efd8b28a723f9b
Matthew Butterick <mb@mbtype.com>
2015-04-03 23:15:29 -0700

    resume in typed container

--- typed/sugar/container.rkt
+++ typed/sugar/container.rkt
@@ -3,7 +3,7 @@
 (require typed/sugar/coerce typed/sugar/len #;racket/list #;racket/set #;racket/sequence #;racket/stream #;racket/dict)
 
 
-(define-type (Containerof A) (U (Listof A) (Vectorof A) String Symbol Path (Sequenceof A)))
+(define-type (Containerof A) (U (Listof A) (Vectorof A) String Symbol HashTableTop Path (Sequenceof A)))
 
 (define/typed+provide (gettable-container? x)
   (Any . -> . Boolean)
@@ -16,36 +16,36 @@
   (for/or ([proc (list list? string? symbol? vector? path? (λ(i) (and (not (hash? i)) (sequence? i))))])
     (proc x)))
 
+(define-predicate index? Index)
 
 (define/typed+provide get
   ;; (U Index A) = get key. Either a numerical index, or hashkey of type A
-  (All (A) (case-> ((HashTable A Any) A (Option Index) . -> . Any)
-                   ((HashTable A Any) A . -> . Any)
-                   ((Containerof A) Index (Option Index) . -> . (U A (Containerof A)))
-                   ((Containerof A) Index . -> . (U A (Containerof A)))))
+  (All (A) (case-> ((Containerof A) (U Index A) (Option Index) . -> . (U A (Containerof A)))
+                   ((Containerof A) (U Index A) . -> . (U A (Containerof A)))))
   (case-lambda
     [(container start)
      (get container start #f)]
     [(container start end)
-     (define result 
-       ;; use handler to capture error & print localized error message
-       (with-handlers ([exn:fail? (λ(exn) (error (format "get: couldn’t retrieve ~a from ~a" (if end (format "items ~a through ~a" start end) (format "item ~a" start)) container)))])
-         (let ([end (if (and (equal? #f end) (sliceable-container? container)) (add1 start) end)])
-           (cond
-             [(hash? container) (hash-ref container start)]
-             [(list? container) (for/list : (Listof A) ([i (in-range start end)]) (list-ref container i))]
-             [(vector? container) (for/vector ([i (in-range start end)]) (vector-ref container i))]
-             [(string? container) (substring container start end)]
-             [(symbol? container) (->symbol (get (->string container) start end))] 
-             [(path? container) (get (explode-path container) start end)] 
-             [(sequence? container) (get (->list container) start end)]
-             [else (error)]))))
-     
-     ;; don't return single-item results inside a list
-     ;; check for integer because integers don't have length
-     (if (and (not (integer? result)) (= (len result) 1) (sliceable-container? container))
-         (car (->list result))
-         result)]))
+     ;; use handler to capture error & print localized error message
+     (with-handlers ([exn:fail? (λ(exn) (error (format "get: couldn’t retrieve ~a from ~a" (if end (format "items ~a through ~a" start end) (format "item ~a" start)) container)))])
+       (if (hash? container)
+           (hash-ref container start)
+           (let ()
+             (define result 
+               (let ([end (if (number? start) (add1 start) end)])
+                 (cond
+                   [(list? container) (for/list : (Listof A) ([i (in-range start end)]) (list-ref container i))]
+                   [(vector? container) (for/vector ([i (in-range start end)]) (vector-ref container i))]
+                   [(string? container) (substring container start end)]
+                   [(symbol? container) (->symbol (get (->string container) start end))] 
+                   [(path? container) (get (explode-path container) start end)] 
+                   [(sequence? container) (get (->list container) start end)]
+                   [else (error)])))
+             ;; don't return single-item results inside a list
+             ;; check for integer because integers don't have length
+             (if (and (not (integer? result)) (= (len result) 1) (sliceable-container? container))
+                 (car (->list result))
+                 result))))]))
 
 #|
 (define (sliceable-container? x)
